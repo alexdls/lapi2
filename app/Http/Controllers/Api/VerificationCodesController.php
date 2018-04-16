90386a0ce4dcf96076a040e29640d805c0c64b66
alex <alex_dls@163.com>
2018-04-16 13:57:28 +0800

    user can register using phone

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -12,8 +12,11 @@ class VerificationCodesController extends Controller
     {
         $phone = $request->phone;
         if(!app()->environment('production')) {
-                $code = '1234';
+                $code = '123456';
         } else {
+            // 生成4位随机数, 左侧补0
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+
             try {
                 $result = $easySms->send($phone, [
                     'content' => '【breeze风】 您的验证码是{$code}。如非本人操作，请忽略本短信'
@@ -25,8 +28,7 @@ class VerificationCodesController extends Controller
             }
         }
 
-        // 生成4位随机数, 左侧补0
-        $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+
 
 
         $key = 'verificationCode_' . str_random(15);
